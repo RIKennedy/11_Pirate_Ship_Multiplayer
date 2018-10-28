6c2a7b528cd88471e3c846f47b19550cee25acb9
Ryan Kennedy <RIKennedy@me.com>
2018-10-28 11:55:47 -0700

    Added new Network connection script

---
+++ Assets/_Scripts/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+    public void MyStartHost()
+    {
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " - Host started");
+    }
+
+    public override void OnStartClient(NetworkClient myclient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " - Client start requested ");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " - Client Connected to IP: " + conn.address);
+    }
+
+
+}
