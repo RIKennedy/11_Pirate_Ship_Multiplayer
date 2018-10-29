2a3e05e03cacf6b8b915bb0c6dd9333acb009a78
Ryan Kennedy <RIKennedy@me.com>
2018-10-28 23:04:08 -0700

    Completed networking with player movement and cameras. COURSE COMPLETE

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+
+
+    private Vector3 inputValue;
+    [SerializeField] float horizScale = 20f;
+    [SerializeField] float vertScale = 20f;
+
+
+
+    void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal") * horizScale * Time.deltaTime;
+        inputValue.y = 0f * Time.deltaTime;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical") * vertScale * Time.deltaTime;
+
+        transform.Translate(inputValue);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+
+
+
+}
